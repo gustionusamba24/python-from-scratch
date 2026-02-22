8affb9afb6071ea644f7da0c2b74519e3a77cfa4
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-02-22 23:55:34 +0700

    working with set in python

--- na12_set_py/main.py
+++ na12_set_py/main.py
@@ -1,6 +1,7 @@
 """
     Set adalah data yang digunakan untuk menampung nilai kolektif unik, jadi tidak ada duplikasi elemen. Elemen yang ada pada set disimpan tidak urut
 """
+from traceback import print_tb
 
 """
     Pengenalan SetL langsung tulis nilai elemen dengan separator `,` dan diapit menggunakan tanda kurung kurawal `{}`
@@ -91,3 +92,104 @@ print(f"len: {len(fellowship)}, data: {fellowship}")
     Copy set
     Method copy() digunakan untuk meng-copy set, menghasilkan data set baru.
 """
+
+"""
+    Pengecekan difference antar set
+    Method difference() digunakan untuk mencari perbedaan elemen antara data (dimana method dipanggil) vs data pada argumen pemanggilan method tersebut.
+"""
+# mencari elemen di real madrid yang tidak ada di man_utd
+real_madrid = {"cristiano ronaldo", "karim benzema", "varane", "casemiro", "luca modric", "carvajal"}
+man_utd = {"casemiro", "cristiano ronaldo", "varane", "marcus rashford", "bruno fernandes"}
+
+diff = real_madrid.difference(man_utd)
+print("diff:", diff)
+
+"""
+    ada juga method difference_update() yang kegunaannya adalah mengubah nilai data (dimana method dipanggil) dengan nilai baru yang didapat dari perbedaan elemen antara data tersebut vs data pada argumen pemanggilan method
+"""
+real_madrid.difference_update(man_utd)
+print("real madrid:", real_madrid)
+
+"""
+    Pengecekan intersection antar set
+    Method intersection() digunakan untuk mencari elemen yang ada di data (dimana method dipanggil) vs data pada argumen pemanggilan method tersebut.
+"""
+fellowship = {'aragorn', 'gimli', 'legolas', 'gandalf', 'boromir', 'frodo', 'sam', 'merry', 'pippin'}
+hobbits = {'frodo', 'sam', 'merry', 'pippin', 'bilbo', 'agus'}
+inter = fellowship.intersection(hobbits)
+print(fellowship)
+
+"""
+    Pengecekan keanggotaan subset
+    Di awal chapter ini kita telah sedikit menyinggung pengecekan membership menggunakan kombinasi keyword if dan in. Selain method tersebut, ada alternatif cara lain yang bisa digunakan untuk mengecek apakah suatu data (yang pada konteks ini adalah set). Caranya menggunakan method issubset()
+"""
+fellowship = {'aragorn', 'gimli', 'legolas', 'gandalf', 'boromir', 'frodo', 'sam', 'merry', 'pippin'}
+hobbits_1 = {'frodo', 'sam', 'merry', 'pippin', 'bilbo'}
+res_1 = hobbits_1.issubset(fellowship)
+print("res_1:", res_1)
+
+hobbits_2 = {'frodo', 'sam', 'merry', 'pippin'}
+res_2 = hobbits_2.issubset(fellowship)
+print("res_2:", res_2)
+
+"""
+    Pengecekan keanggotaan superset
+    Selain issubset(), ada juga issuperset() yang fungsinya kurang lebih sama namun kondisinya pengecekannya dibalik.
+"""
+fellowship = {'aragorn', 'gimli', 'legolas', 'gandalf', 'boromir', 'frodo', 'sam', 'merry', 'pippin'}
+hobbits_1 = {'frodo', 'sam', 'merry', 'pippin', 'bilbo'}
+
+res_1 = fellowship.issuperset(hobbits_1)
+print("res_1:", res_1)
+
+""" 
+    Pengecekan keanggotaan disjoint
+    method ini mengembalikan nilai True jika set pada pemanggilan fungsi berisi elemen yang semuanya bukan anggota data dimana method dipanggil.
+"""
+fellowship = {'aragorn', 'gimli', 'legolas', 'gandalf', 'boromir', 'frodo', 'sam', 'merry', 'pippin'}
+
+res_1 = fellowship.isdisjoint({"aragorn", "gimli"})
+print("res_1:", res_1)
+
+res_2 = fellowship.isdisjoint({'pippin', 'bilbo'})
+print("res_2:", res_2)
+
+res_3 = fellowship.isdisjoint({'bilbo'})
+print("res_3:", res_3)
+
+"""
+    Operator bitwise pada set
+    Operasi or pada set menggunakan operator |
+    Berisi kombinasi antar 2 elemen
+"""
+a = set("abracadabra")
+b = set("alacazam")
+
+res = a | b
+print(res)
+
+# operasi and pada set menggunakan &
+# berisi elemen set yang merupakan anggota set a dan set b. Operasi ini biasa disebut dengna operasi and
+a = set("abracadabra")
+b = set("alacazam")
+
+res = a & b
+print(res)
+
+# operasi exclusive or pada set menggunakan operator ^
+# berisi elemen set yang ada di set a atau b tetapi tidak ada di keduanya
+a = set('abracadabra')
+b = set('alacazam')
+
+res = a ^ b
+print(res)
+
+# Operator `-` pada set
+# Digunakan untuk pencarian perbedaan elemen. Contoh penerapan
+a = set('abracadabra')
+b = set('alacazam')
+
+res_1 = a - b
+res_2 = b - a
+print(res_1)
+print(res_2)
