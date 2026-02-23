4525d6f3daa3176c6a0ec7a9592018c08ad84f46
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-02-23 15:52:01 +0700

    working with dictionary

---
+++ na13_dict_py/main.py
@@ -0,0 +1,9 @@
+"""
+    Dictionary atau dict adalah tipe data kolektif berbentuk key-value. Contoh penulisannya:
+"""
+profile = {
+    "id": 2,
+    "name": "john wick",
+    "hobbies": ["playing with pencil"],
+    "is_female": False
+}
