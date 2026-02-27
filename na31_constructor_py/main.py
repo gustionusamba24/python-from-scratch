b09c0243334e92524dad4b5a5299d443be5aaa70
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-02-27 14:37:12 +0700

    working with oop -> constructor in python

---
+++ na31_constructor_py/main.py
@@ -0,0 +1,3 @@
+"""
+    Constructor adalah salah satu topik penting dalam pemrograman berbasis OOP. Konstruktor adalah fungsi khusus yang dipanggil saat pembuatan object dilakukan dari suatu class.
+"""
