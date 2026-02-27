3d2120093b6dddb1355f5d7ef2938f5a9587fd12
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-02-27 23:19:17 +0700

    working with oop -> inheritance

---
+++ na37_inheritance_py/Vehicle.py
@@ -0,0 +1,45 @@
+class Vehicle:
+    note = "class to represent a Car"
+
+    def __init__(self):
+        self.name = "common vehicle"
+        self.number_of_wheels = 4
+
+    def drive_sound(self):
+        return "vroom vroom"
+
+
+class ElectricCar(Vehicle):
+    def __init__(self):
+        super().__init__()
+        self.name = "electric car"
+
+    def info(self):
+        print(self.name, "has", self.number_of_wheels, "wheels. engine sound:", self.drive_sound())
+
+    def drive_sound(self):
+        return "zzzzzzz"
+
+
+v1 = Vehicle()
+print(v1.name, "has", v1.number_of_wheels, "wheels. engine sound:", v1.drive_sound())
+
+v2 = ElectricCar()
+print(v2.name, "has", v2.number_of_wheels, "wheels. engine sound:", v2.drive_sound())
+
+v3 = ElectricCar()
+v3.info()
+print()
+
+if isinstance(v1, Vehicle):
+    print("v1 class inherit from class Vehicle")
+if isinstance(v1, object):
+    print("v1 class inherit from class object")
+
+v2 = ElectricCar()
+if isinstance(v2, ElectricCar):
+    print("v2 class inherit from class ElectricCar")
+if isinstance(v2, Vehicle):
+    print("v2 class inherit from class Vehicle")
+if isinstance(v2, object):
+    print("v2 class inherit from class object")
