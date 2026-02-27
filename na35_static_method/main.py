50935cc5e9e1008e3b8e5aa1f4cb9e6f05675086
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-02-27 21:51:04 +0700

    working with oop -> static method

---
+++ na35_static_method/main.py
@@ -0,0 +1,5 @@
+"""
+    Selain dua method tersebut, ada lagi jenis method lain bernama static method atau method statis, yang ciri khasnya adalah memiliki decorator @staticmethod dan tidak memiliki parameter implicit self maupun cls.
+
+    Static method bisa diakses via instance object maupun via class. Contoh penerapannya bisa dilihat pada kode berikut:
+"""
