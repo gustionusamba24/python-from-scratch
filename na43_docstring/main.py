a4e9f51ab713abfab74c20852fc783d9042d1947
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-03-01 16:20:44 +0700

    docstring in python

---
+++ na43_docstring/main.py
@@ -0,0 +1,6 @@
+from quote import Quote, print_random_quote
+
+if __name__ == "__main__":
+    print_random_quote()
+
+    Quote.print_quote(2)
---
+++ na43_docstring/quote.py
@@ -0,0 +1,41 @@
+quotes = [
+    "never let anyone live in your head rent free",
+    "if others can do it, then why should I?",
+    "\n".join([
+        "I'm sick of following my dreams, man.",
+        "I'm just going to ask where they're going and hook up with 'em later."
+    ]),
+]
+
+from random import randint
+
+
+def print_random_quote():
+    """
+    function `print_random_quote()`:
+        print one random quote,
+        so nothing special
+    """
+
+    i = randint(0, len(quotes) - 1)
+    print(quotes[i])
+
+
+class Quote:
+    """
+    class `Quote`:
+        A class Quote represent a quote.
+        It has the following two attributes:
+            - class attribute `note`
+            - instance method `print_quote()`
+    """
+
+    note = "A class to represent quote"
+
+    @classmethod
+    def print_quote(cls, i):
+        """
+        instance method `print_quote()`:
+            Responsible to print specific quote by index
+        """
+        print(quotes[i])
