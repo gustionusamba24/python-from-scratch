1dda7dc00d669d993e117919f325cf2aeff9aafb
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-03-03 12:56:35 +0700

    delete object

---
+++ na49_delete_object/main.py
@@ -0,0 +1,6 @@
+"""
+    del digunakan untuk operasi delete object. Hasil penggunaan keyword del benar-benar menghapus variabel beserta isinya, jadi tidak hanya mengosongkan nilainya
+"""
+"""
+    Penggunaan None sebagai nilai suatu variabel menjadikan variabel tsb tetap ada namun nilainya berubah menjadi None. Jadi, penggunaan None berguna untuk mengosongkan variabel tanpa menghapus variable itu sendiri.
+"""
