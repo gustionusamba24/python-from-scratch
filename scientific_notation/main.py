6831c9c650a7b9187b0599bcae536164f5c91054
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-05-06 23:22:13 +0700

    scientific notation

---
+++ scientific_notation/main.py
@@ -0,0 +1,9 @@
+small_number = 1.024e18
+medium_number = 1.024e19
+large_number = 1.024e20
+
+print(f"small number: {small_number}, medium number: {medium_number}, large number: {large_number}")
+print(f"small number: {small_number:.4e}, medium number: {medium_number:.4e}, large number: {large_number:.4e}")
+print(f"small number: {small_number:.0f}, medium number: {medium_number:.0f}, large number: {large_number:.0f}")
+print(f"small number: {small_number:,.0f}, medium number: {medium_number:,.0f}, large number: {large_number:,.0f}")
+
