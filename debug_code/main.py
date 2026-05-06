71aff9515ee7fc6011b731c42f5ef7c49d1d9d8f
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-05-06 19:30:32 +0700

    debug python in advance

--- debug_code/main.py
+++ debug_code/main.py
@@ -6,3 +6,26 @@ def sum_numbers(*numbers):
     return total
 
 print(sum_numbers(1, 2, 3, 4, 5))
+
+def matrix_multiply_scalar(matrix, scalar=1):
+    res = []
+    for row in matrix:
+        res.append([cell * scalar for cell in row])
+    return res
+
+matrix = [
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12],
+]
+
+print(f"matrix * scalar {1}:")
+res1 = matrix_multiply_scalar(matrix)
+for el in res1:
+    print(el)
+
+    
+print(f"matrix * scalar {5}:")
+res2 = matrix_multiply_scalar(matrix, scalar=5)
+for el in res2: 
+    print(el)
