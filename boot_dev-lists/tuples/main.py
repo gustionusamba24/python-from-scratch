9ca206875bbc3c59877d270b490771c763820f62
Gustio Nusamba <gustio.n@students.amikom.ac.id>
2026-05-10 14:09:06 +0700

    list: tuple

---
+++ boot_dev-lists/tuples/main.py
@@ -0,0 +1,11 @@
+def get_heroes():
+    heroes = [
+        ("Ironman", "Tony Stark", 1000),
+        ("Captain America", "Steve Rogers", 900),
+        ("Thor", "Thor Odinson", 1100),
+        ("Hulk", "Bruce Banner", 1200),
+        ("Black Widow", "Natasha Romanoff", 800),
+        ("Hawkeye", "Clint Barton", 700),
+    ]
+
+    return heroes
---
+++ boot_dev-lists/tuples/main_test.py
@@ -0,0 +1,28 @@
+from main import get_heroes
+
+run_cases = [
+    (
+        [
+            (
+                "Glorfindel",
+                2093,
+                True,
+            ),
+            (
+                "Gandalf",
+                1054,
+                False,
+            ),
+            (
+                "Gimli",
+                389,
+                False,
+            ),
+            (
+                "Aragorn",
+                87,
+                False,
+            ),
+        ]
+    ),
+]
